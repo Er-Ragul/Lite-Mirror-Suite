be89412174bfa7610e4ae70cdf8f1d73d4234012
Ragul.H <45266281+Er-Ragul@users.noreply.github.com>
2021-05-11 10:26:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+})
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+      }
+})
+
+let softid
+
+app.use('/peerjs', peerServer)
+app.set('view-engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/client', (req, res) => {
+    res.render('client.ejs')
+});
+
+app.get('/engine', (req, res) => {
+    res.render('engine.ejs')
+});
+
+io.on('connection', socket => {
+    io.to(socket.id).emit('YourId', socket.id);
+    console.log('Client Connected id : ' + socket.id)
+
+    // Need to work on this soon //
+    socket.on('reserve_id', (data) => {
+        softid = data.id
+        console.log('Software ID : ' + softid)
+    })
+
+    socket.on('makeCall', () => {
+        io.to(softid).emit('makeClientCall', '143');
+    })
+})
+
+server.listen(process.env.PORT || 3000)

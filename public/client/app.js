8f6515a38f0c5e084831a50a2ef3abb9705856ba
Ragul.H <45266281+Er-Ragul@users.noreply.github.com>
2021-05-14 10:38:04 +0530

    Add files via upload

---
+++ public/client/app.js
@@ -0,0 +1,133 @@
+/* Global Variables */
+let token
+let peer
+let dc
+let ms_width = 1920
+let ms_height = 1080
+let divided_width
+let divided_height
+let setWidth
+let setHeight
+let count = 0
+
+/* Import or Initilization */
+const socket = io.connect('/')
+const display = document.getElementById('display')
+const tokenBox = document.getElementById('tokenBox') 
+const maxBtn = document.getElementById('maxBtn')
+
+/* Connection Establishment Event */
+socket.on('YourId', (myId) => {
+    document.getElementById('btn').disabled = false
+})
+
+/* Connection Establishment Function */
+const startConnection = () => {
+    token = document.getElementById('token').value
+    if(token) { 
+        tokenBox.remove()
+        socket.emit('makeCall', token) 
+        peer = new Peer(token.toString(), {
+            host: 'localhost',
+            port: 443,
+            path: '/peerjs',
+            secure: true,
+            config: {
+                'iceServers': [
+                       { url: 'stun:stun1.l.google.com:19302' },
+                       {
+                           url: 'turn:3.131.158.239:3478?transport=udp',
+                           credential: 'ragul',
+                           username: 'ragul'
+                       }]
+            }
+        })
+        receiveShare() 
+    }
+    else { window.alert('Token should not be empty... Try Again') }
+}
+
+/* Initializing display function */
+const receiveShare = () => {
+    peer.on('call', (call) => {
+        dc = peer.connect('software')
+        console.log('Incoming Call')
+        call.answer(null)
+        call.on('stream', (stream) => {
+            count++
+            if(count === 2){
+                createDisplay(stream)
+            }
+            else {
+                console.log(`Stream received for ${count} times`)
+            }
+        })
+
+        dc.on('open', () => {
+            dc.on('data', (msg) => {
+                console.log(msg)
+            })
+        })
+    })
+}
+
+/* Function to create display and sizing */
+const createDisplay = (stream) => {
+    divided_width = ms_width / window.innerWidth
+    divided_height = ms_height / window.innerHeight
+    setWidth = ms_width / Math.round(divided_width)
+    setHeight = ms_height / Math.round(divided_height)
+    console.log('Video Width : ', setWidth)
+    console.log('Vidoe Height : ', setHeight)
+    display.width = setWidth
+    display.height = setHeight
+    display.muted = true
+    display.style.display = "block"
+    maxBtn.style.display = "block"
+    display.srcObject = stream
+    display.play()
+}
+
+/* Mouse onclick event function */
+const mouseClick = (e) => {
+    let posX = display.offsetLeft
+    let posY = display.offsetTop
+    let tempX = (e.pageX - posX) / setWidth * 100 
+    let tempY = (e.pageY - posY) / setHeight * 100
+    /*-----------------------------*/
+    let X = tempX / 100 * ms_width
+    let Y = tempY / 100 * ms_height
+    /*-----------------------------*/
+    let pointer = {x: Math.floor(X), y: Math.floor(Y)}
+    console.log(pointer)
+    dc.send(pointer)
+}
+
+/* Keyboard event function */
+const keyEvent = (e) => {
+    var keys = e.which || e.keyCode;
+    try {
+        dc.send(String.fromCharCode(keys))
+    } catch (error) {
+        console.log('Peer not initiated')
+    }
+}
+
+/* Display full screen size setter function */
+const fullScreen = () => {
+    if(display.requestFullscreen)
+    {
+        display.requestFullscreen();
+        display.controls = false;
+    } 
+    else if (display.mozRequestFullScreen) 
+    {
+        display.mozRequestFullScreen();
+        display.controls = false;
+    } 
+    else if (display.webkitRequestFullscreen) 
+    {
+        display.webkitRequestFullscreen();
+        display.controls = false;
+    }
+}
---
+++ public/engine/source.js
@@ -0,0 +1,109 @@
+/* Global Variables */
+let displaySource
+let peer
+let dc
+
+/* Import or Initialization */
+let socket = io.connect('/')
+const { desktopCapturer } = require('electron')
+const fs = require('fs')
+const { type } = require('os')
+desktopCapturer.getSources({ types: ['window', 'screen'] })
+    .then(sources => {
+        for(i=0; i<sources.length; i++){
+            if(sources[i].name === 'Entire Screen'){
+                displaySource = sources[i].id
+            }
+        }
+    })
+    .catch(e => console.log(e))
+
+
+/* Socket callback events for peer to peer connectivity */
+socket.on('YourId', (myId) => {
+    console.log('Received ID from server')
+    peer = new Peer('software', {
+        host: 'localhost',
+        port: 443,
+	    path: '/peerjs',
+        secure: true,
+        config: {
+            'iceServers': [
+                   { url: 'stun:stun1.l.google.com:19302' },
+                   {
+                       url: 'turn:3.131.158.239:3478?transport=udp',
+                       credential: 'ragul',
+                       username: 'ragul'
+                   }]
+        }
+    })
+    dataConnection()
+    socket.emit('reserve_id', {name: 'software', id: myId})
+})
+
+/* Make call to client  ---> step : 1 */
+socket.on('makeClientCall', (client_id) => {
+    console.log('Call Initiated from client')
+    startShare(client_id)
+})
+
+/* Function to call client with media stream  ---> step : 2 */
+function startShare(client_id){
+    navigator.mediaDevices.getUserMedia({video: 
+        {
+            mandatory: {              
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: displaySource
+            },
+            cursor: 'never'
+        }, 
+        audio: {
+            mandatory: {
+                chromeMediaSource: 'desktop'
+            }
+        }
+        })
+        .then(stream => {
+            peer.call(client_id, stream)
+        })
+        .catch(e => console.log(e))
+}
+
+/* Function to receive data's (mouse coordinates) from client */
+const dataConnection = () => {
+    peer.on('connection', function(conn) {
+        dc = conn
+        dc.on('open', () => {
+            dc.on('data', (pointer) => {
+                click(pointer)
+            })
+        })
+    })
+}
+
+/* Mouse Click (Single click) function */
+const click = (command) => {
+    if(typeof(command) === "object"){
+        console.log(typeof(command))
+        console.log('X : ' + command.x)
+        console.log('Y : ' + command.y)                    
+        var mouseCoordinates = command.x + ',' + command.y
+        fs.writeFile('./interface/mouse_pointer.txt', mouseCoordinates, function (err) {
+            if (err) throw err;
+            console.log('Mouse pointer updated !');
+        });
+    }
+    else if(typeof(command) === "string"){
+        console.log('Keys :', command)
+        var pencil = 'write' + ',' + command
+        fs.writeFile('./interface/key_events.txt', pencil, function (err) {
+            if (err) throw err;
+            console.log('String updated !');
+        });
+    }
+}
+
+/* Message sender function */
+const send = () => {
+    dc.send('Message sent from Software')
+}
